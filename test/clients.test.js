a29221dac63695e30efec11ef96d2f6ca5520b6a
Kevin Grayson <k.grayson.ms@gmail.com>
2018-07-09 20:29:41 -0700

    Test for username assignment

--- test/clients.test.js
+++ test/clients.test.js
@@ -20,10 +20,18 @@ describe('Clients', () => {
         assert.deepEqual(allClients, [c1, c2, c3]);
     });
 
+    it('Assigns a username', () => {
+        assert.equal(c1.username, 'User#1');
+        assert.equal(c2.username, 'User#2');
+        assert.equal(c3.username, 'User#3');
+    });
+
     it('Removes a client', () => {
         clients.remove(c2);
         const allClients = clients.getAllClients();
         assert.deepEqual(allClients, [c1, c3]);
     });
+
+
 
 });
